c0f12cc7dd21727db8262b3d5674054b76e0ccba
Thilan Senanayake <thilansenanayake@gmail.com>
2021-01-24 16:20:46 +0530

    Added form to add new expenses

--- expense_tracker/tracker/views.py
+++ expense_tracker/tracker/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Expense
+from .forms import ExpenseForm
 
 def home(request):
-  expenses = Expense.objects.all()
-  context = { 'expenses': expenses }
+  if request.method == 'POST':
+    form = ExpenseForm(request.POST)
+    form.instance.user = request.user
+
+    if form.is_valid():
+      form.save()
+      return redirect('tracker-home')
+
+  else:
+    form = ExpenseForm()
+
+  expenses = Expense.objects.order_by('-date_of_expenditure')
+  context = { 'form': form, 'expenses': expenses }
   return render(request, 'tracker/home.html', context)
