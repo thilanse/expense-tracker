4f7a69687f246818c499bd447c8bdd12cf33ade6
Thilan Senanayake <thilansenanayake@gmail.com>
2021-01-24 17:52:18 +0530

    Implemented update and delete functionality

--- expense_tracker/tracker/urls.py
+++ expense_tracker/tracker/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='tracker-home')
+  path('', views.home, name='tracker-home'),
+  path('expense/<int:pk>/update', views.update_expense, name='expense-update'),
+  path('expense/<int:pk>/delete', views.delete_expense, name='expense-delete')
 ]
--- expense_tracker/tracker/views.py
+++ expense_tracker/tracker/views.py
@@ -18,4 +18,30 @@ def home(request):
 
   expenses = Expense.objects.order_by('-date_of_expenditure')
   context = { 'form': form, 'expenses': expenses }
-  return render(request, 'tracker/home.html', context)
+  return render(request, 'tracker/expense_list.html', context)
+
+def update_expense(request, pk):
+  expense_to_update = Expense.objects.get(id=pk)
+  if request.method == 'POST':
+    update_form = ExpenseForm(request.POST, instance=expense_to_update)
+
+    if update_form.is_valid():
+      update_form.save()
+      return redirect('tracker-home')
+  else:
+    update_form = ExpenseForm(instance=expense_to_update)
+
+  form = ExpenseForm()
+  expenses = Expense.objects.order_by('-date_of_expenditure')
+  context = { 
+    'expenses': expenses, 
+    'update_id': pk, 
+    'update_form': update_form,
+    'form': form
+  }
+  return render(request, 'tracker/expense_update.html', context)
+
+def delete_expense(request, pk):
+  expense_to_delete = Expense.objects.get(id=pk)
+  expense_to_delete.delete()
+  return redirect('tracker-home')
