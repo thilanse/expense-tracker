77868a28819e016a5eb338938d26c9a04ee31640
Thilan Senanayake <thilansenanayake@gmail.com>
2021-01-28 17:24:12 +0530

    Added styling to expense list

--- expense_tracker/tracker/forms.py
+++ expense_tracker/tracker/forms.py
@@ -5,17 +5,19 @@ class ExpenseForm(forms.ModelForm):
 
   class Meta:
     model = Expense
-    fields = ['amount', 'reason']
+    fields = ['amount', 'reason', 'date_of_expenditure']
     widgets = {
             'amount': forms.NumberInput(attrs={'class': 'form-control'}),
             'reason': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_of_expenditure': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
         }
 
 class ExpenseUpdateForm(forms.ModelForm):
 
-  amount = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control mb-2 mr-sm-2'}), label='')
-  reason = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-2 mr-sm-2'}), label='')
+  amount = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'update-form-field'}), label='')
+  reason = forms.CharField(widget=forms.TextInput(attrs={'class': 'update-form-field'}), label='')
+  date_of_expenditure = forms.DateField(widget=forms.DateInput(attrs={'class': 'update-form-field', 'type': 'date'}), label='')
 
   class Meta:
     model = Expense
-    fields = ['amount', 'reason']
+    fields = ['amount', 'reason', 'date_of_expenditure']
