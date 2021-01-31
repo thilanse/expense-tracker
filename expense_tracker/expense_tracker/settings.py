7520115a4a34f4eb7ae5807642e7dd0bbfaaf2c1
Thilan Senanayake <thilansenanayake@gmail.com>
2021-01-31 22:12:21 +0530

    Added registration form

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tracker.apps.TrackerConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'tracker-home'
+LOGIN_URL = 'login'
